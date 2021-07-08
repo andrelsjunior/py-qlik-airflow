0ca8918ec8d193a205bce62c20dc9bbe3d163c4c
André Junior <andreqrqp@gmail.com>
2021-07-08 01:50:19 -0300

    Update qlik.py

--- qlik.py
+++ qlik.py
@@ -2,19 +2,20 @@
 import json
 from qsaas.qsaas import Tenant
 
-# Info needed to auth
+# Informações necessárias para autenticação
+# Chave API
 key = 'Qlik API KEY'
-
+# Domínio completo
 fqdn = 'Qlik full domain name'
+# ID de usuário
+usr_id = 'Qlik UserId'
 
-id = 'Qlik UserId'
+# Qlik connection
+qconn = Tenant(api_key=key, tenant=fqdn, tenant_id=usr_id)
 
-# Qlik conn
-q = Tenant(api_key=key, tenant=fqdn, tenant_id=id)
-
-# To send a reload request to app
-def reload(q, app_id):
-    q.post('reloads', json.dumps({'appID': f'{app_id}'}))
+# Função para enviar um request de recarga à aplicação
+def reload(qlik_connection, app_id):
+    qlik_connection.post('reloads', json.dumps({'appID': f'{app_id}'}))
 
 # Sending reload request
-reload(app_id, q)
+reload(app_id, qconn)
